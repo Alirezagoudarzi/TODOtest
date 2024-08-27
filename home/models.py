b0ffe714b01d547ccb3937b98fa3ff93f66a0adc
Alirezagoudarzi <arg_7ty7@yahoo.com>
2024-08-27 22:10:13 +0330

    TODO table create and run successfully

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class TODO(models.Model):
+    title=models.CharField(max_length=100)
+    body=models.CharField(max_length=200)
+    created= models.DateField()
+
+
+
